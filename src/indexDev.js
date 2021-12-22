26c1f1eb58278f0b76d78d33a8f8ffd4ea16dbc9
Luis Mendoza <lmendoza.mg@gmail.com>
2021-12-22 18:41:20 +0100

    fixed untracked files

--- src/indexDev.js
+++
@@ -1,9 +0,0 @@
-import { App } from "./classes/App.js"
-import { Metro } from "./classes/Metro.js"
-import { network } from "./data/network.js"
-
-const metro = new Metro(network)
-
-metro.init()
-
-console.log(metro.shortestPath({from: "C", to: "F", color: "Sin color"}))
